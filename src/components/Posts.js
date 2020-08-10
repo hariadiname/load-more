cee6f2ad7d6e35f9b288503b1dade52a16bdde12
hariadi1711 <hariadiii1711@gmail.com>
2020-08-11 07:31:05 +0800

    load more fitur

---
+++ src/components/Posts.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import Axios from "axios";
+
+export default class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      page: 1,
+      limit: 5,
+    };
+    this.loadMore = this.loadMore.bind(this);
+  }
+
+  URL_LPTQ = "http://api.lptqpapua.org/api/portal/post?";
+
+  componentDidMount() {
+    Axios.get(
+      `${this.URL_LPTQ}page=${this.state.page}&limit=${this.state.limit}`
+    )
+      .then((res) => {
+        this.setState({
+          items: res.data.data.posts,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  loadMore() {
+    this.setState((old) => {
+      return { limit: old.limit + 5 };
+    });
+
+    Axios.get(
+      `${this.URL_LPTQ}page=${this.state.page}&limit=${this.state.limit}`
+    )
+      .then((res) => {
+        this.setState({
+          items: res.data.data.posts,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  render() {
+    return (
+      <div className="wrapper-loadmore">
+        <h1>Data Posts</h1>
+        {this.state.items
+          .slice(0, this.state.limit)
+          .map(({ id, author, title, created_at, url }) => {
+            return (
+              <div className="card" key={id}>
+                <h1>{title}</h1>
+                <div className="wrapper-text">
+                  <h5>{author.name}</h5>|<h5>{created_at}</h5>
+                </div>
+                <a href={url}>baca</a>
+              </div>
+            );
+          })}
+        {this.state.limit >= this.state.items.length && (
+          <button type="button" onClick={this.loadMore}>
+            load more
+          </button>
+        )}
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Posts from "./components/Posts";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Posts />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
